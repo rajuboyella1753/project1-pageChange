48bbaff8d3de92f79ab18c71d4f6d876cf87701c
rajuboyella1753 <rajuboyella737@gmail.com>
2024-06-11 16:16:53 +0530

    Fully designed website
    ready for host

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ real.addEventListener('click' ,click);
 function click(){
   document.body.style.backgroundColor = "green";
 }
+
+let real1 = document.getElementById("check1");
+real1.addEventListener('click' ,click1);
+function click1(){
+  document.body.style.backgroundColor ="yellow";
+}
